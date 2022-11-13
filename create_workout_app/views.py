e3dcc1fe9658d0f8a604c57f3c12ffefbe0ad427
Matt <mjd014@live.co.uk>
2022-11-13 16:07:20 +0000

    created views for WorkoutList, ExerciseList, CreateWorkout and CreateExercise.

--- create_workout_app/views.py
+++ create_workout_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Workout, Exercise
+from django.views import generic
 
-# Create your views here.
+
+class WorkoutList(generic.ListView):
+
+    model = Workout
+    template_name = 'index.html'
+
+
+class ExerciseList(generic.ListView):
+
+    model = Exercise
+    template_name = 'exercise_list.html'
+
+
+class CreateWorkout(generic.CreateView):
+
+    model = Workout
+    template_name = 'create_workout.html'
+
+
+class CreateExercise(generic.CreateView):
+
+    model = Exercise
+    template_name = 'create_exercise.html'
+
+
