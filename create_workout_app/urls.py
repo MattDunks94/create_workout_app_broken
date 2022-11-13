ea89a00bf680124515ec20769d76013e80e35c9e
Matt <mjd014@live.co.uk>
2022-11-13 16:06:24 +0000

    added paths to 'home' WorkoutList view and 'exercise_list' ExerciseList view.

--- create_workout_app/urls.py
+++ create_workout_app/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
+    path('', views.WorkoutList.as_view(), name='home'),
+    path('exercise_list/', views.ExerciseList.as_view(), name='exercise_list'),
 ]
