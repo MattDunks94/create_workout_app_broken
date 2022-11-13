c937fbdb5d15931c7dc3deb527b9cbe0233e00e5
Matt <mjd014@live.co.uk>
2022-11-13 18:16:43 +0000

    extended on create views for both workout and exercise models.

--- create_workout_app/views.py
+++ create_workout_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import Workout, Exercise
 from django.views import generic
 
@@ -18,12 +19,24 @@ class ExerciseList(generic.ListView):
 class CreateWorkout(generic.CreateView):
 
     model = Workout
+    fields = ['title', 'featured_image']
     template_name = 'create_workout.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.published_by = self.request.user
+        return super().form_valid(form)
 
 
 class CreateExercise(generic.CreateView):
 
     model = Exercise
+    fields = ['exercise', 'sets', 'reps', 'weight', 'workout']
     template_name = 'create_exercise.html'
+    success_url = reverse_lazy('exercise_list')
 
-
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        workout = Workout.slug
+        return super().form_valid(form)
+        form.save(workout)
