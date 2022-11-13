bef3744b310e009b20959bf1309514ff172ef9c8
Matt <mjd014@live.co.uk>
2022-11-13 18:16:04 +0000

    added new path to create exercise.

--- create_workout_app/urls.py
+++ create_workout_app/urls.py
@@ -5,4 +5,11 @@ from django.urls import path
 urlpatterns = [
     path('', views.WorkoutList.as_view(), name='home'),
     path('exercise_list/', views.ExerciseList.as_view(), name='exercise_list'),
+    path(
+        'create_workout/', views.CreateWorkout.as_view(), name='create_workout'
+    ),
+    path(
+        'create_exercise/', views.CreateExercise.as_view(), name='create_exercise'
+    ),
+    
 ]
