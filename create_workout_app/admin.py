b61bdc6d69e30dd859c7173a35507bd33f4aeba1
Matt <mjd014@live.co.uk>
2022-11-13 15:38:50 +0000

    registered models to admin site.

--- create_workout_app/admin.py
+++ create_workout_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Workout, Exercise
+
+
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'published_by', 'created_on')
+    list_filter = ('created_on', 'updated_on')
+    search_fields = ['title', 'published_by']
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+
+    list_display = ('exercise', 'workout', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ['exercise', 'workout']
 
 # Register your models here.
