c983e7eebe0197cf9726679d591c9e46ab9c9e59
Matt <mjd014@live.co.uk>
2022-11-14 20:17:08 +0000

    removed path to exercise_list, deleted html file.

--- create_workout_app/urls.py
+++ create_workout_app/urls.py
@@ -4,8 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('', login_required(views.WorkoutList.as_view()), name='home'),
-    path('exercise_list/', views.ExerciseList.as_view(), name='exercise_list'),
+    path('', login_required(views.ExerciseList.as_view()), name='home'),
     path(
         'create_workout/', views.CreateWorkout.as_view(), name='create_workout'
     ),
